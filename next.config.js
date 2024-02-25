1c0528c1dc37b85304150664a8f6ca0b74ce0bf7
hhatakeyama <heitor_suh@hotmail.com>
2024-02-24 23:12:46 -0300

    Ajuste header axios

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
   basePath: '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
-    loader: 'cloudinary',
-    domains: ['localhost', 'admin.gatacompleta.com', 'dev.gatacompleta.com', 'gatacompleta.com'],
-    path: 'https://res.cloudinary.com/gatacompleta/image/fetch/'
+    domains: ['localhost', 'skedyou.com'],
   },
 }
 
