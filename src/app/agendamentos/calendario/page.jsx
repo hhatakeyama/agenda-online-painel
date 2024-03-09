9ef020f379d3e4bf406bb45947e224c18c2985a8
hhatakeyama <heitor_suh@hotmail.com>
2024-03-09 16:40:54 -0300

    Implementado filtro de serviço e funcionários

--- src/app/agendamentos/calendario/page.jsx
+++ src/app/agendamentos/calendario/page.jsx
@@ -2,13 +2,14 @@
 
 import '@mantine/dates/styles.css';
 
-import { Button, Center, Container, Divider, Grid, Group, LoadingOverlay, Paper, rem, ScrollArea, Select, Stack, Table, Text, TextInput } from '@mantine/core'
+import { Button, Center, Container, Divider, Grid, Group, LoadingOverlay, Modal, MultiSelect, Paper, rem, ScrollArea, Select, Stack, Table, Text, TextInput } from '@mantine/core'
 import { DatePicker } from '@mantine/dates'
 import { IconSearch } from '@tabler/icons-react'
 import { redirect } from 'next/navigation'
 import { Fragment, useEffect, useState } from 'react'
 
 import * as Display from '@/components/display'
+import { FormSchedule } from '@/components/forms';
 import guardAccount from '@/guards/AccountGuard'
 import { useFetch } from '@/hooks'
 import { useAuth } from '@/providers/AuthProvider'
@@ -29,6 +30,8 @@ function Calendar() {
   const [searchFilter, setSearchFilter] = useState('')
   const [searchOrganization, setSearchOrganization] = useState(userData?.organization_id || '')
   const [searchCompany, setSearchCompany] = useState('')
+  const [searchServices, setSearchServices] = useState([])
+  const [searchEmployees, setSearchEmployees] = useState([])
   const [date, setDate] = useState(today)
   const [register, setRegister] = useState(false)
 
@@ -38,9 +41,35 @@ function Calendar() {
   const optionsCompanies =
     resultsCompanies.map(company => ({ label: company.name, value: company.id.toString() })) || []
 
+  const { data: dataServices, error: errorServices, mutate: mutateServices } = useFetch([
+    isAuthenticated ? `/admin/services` : null,
+    {
+      organization_id: searchOrganization,
+      ...(optionsCompanies.length > 1 && searchCompany ? { company: searchCompany } : {})
+    }
+  ])
+  const { data: resultsServices = [] } = dataServices?.data || {}
+  const optionsServices =
+    resultsServices.map(employee => ({ label: employee.name, value: employee.id.toString() })) || []
+
+  const { data: dataEmployees, error: errorEmployees, mutate: mutateEmployees } = useFetch([
+    isAuthenticated ? `/admin/employees` : null,
+    {
+      organization_id: searchOrganization,
+      ...(searchServices ? { services: searchServices } : {})
+    }
+  ])
+  const { data: resultsEmployees = [] } = dataEmployees?.data || {}
+  const optionsEmployees =
+    resultsEmployees.map(employee => ({ label: employee.name, value: employee.id.toString() })) || []
+
   const { data: dataSchedules, error: errorSchedules, mutate: mutateSchedules } = useFetch([
     isAuthenticated ? '/admin/schedules/calendar' : null,
-    { search: searchFilter, date: dateToDatabase(date), ...(searchCompany ? { company: searchCompany } : {}) }
+    {
+      search: searchFilter,
+      date: dateToDatabase(date), ...(searchCompany ? { company: searchCompany } : {}),
+      ...(searchEmployees ? { employees: searchEmployees } : {})
+    }
   ])
   const { data: results = [] } = dataSchedules || {}
   const loading = !data && !error
@@ -66,136 +95,163 @@ function Calendar() {
   if (isAuthenticated === true && permissionsData && !permissionsData.ge) return redirect('/agendamentos')
 
   return (
-    <Container size="100%" mb="50px">
-      <Stack>
-        <Group justify="space-between">
-          <Text>Agendamentos</Text>
+    <>
+      <Container size="100%" mb="50px">
+        <Stack>
+          <Group justify="space-between">
+            <Text>Agendamentos</Text>
 
-          <Button onClick={() => setRegister(true)}>Adicionar Agendamento</Button>
-        </Group>
-        <Center>
-          <Paper shadow="sm" style={{ backgroundColor: '#333333' }}>
-            <DatePicker
-              value={date}
-              maxLevel="month"
-              minDate={today}
-              onChange={setDate}
-            />
-          </Paper>
-        </Center>
-        <Grid>
-          {permissionsData?.sa && (
-            <Grid.Col span={{ base: 12, sm: 6, md: 4 }}>
-              <Select
-                placeholder="Empresa"
-                data={optionsOrganizations}
-                value={searchOrganization}
-                onChange={setSearchOrganization}
-                searchable
-                clearable
+            <Button onClick={() => setRegister(true)}>Adicionar Agendamento</Button>
+          </Group>
+          <Center>
+            <Paper shadow="sm" style={{ backgroundColor: '#333333' }}>
+              <DatePicker
+                value={date}
+                maxLevel="month"
+                minDate={today}
+                onChange={setDate}
+              />
+            </Paper>
+          </Center>
+          <Grid>
+            {permissionsData?.sa && (
+              <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }}>
+                <Select
+                  placeholder="Empresa"
+                  data={optionsOrganizations}
+                  value={searchOrganization}
+                  onChange={setSearchOrganization}
+                  searchable
+                  clearable
+                />
+              </Grid.Col>
+            )}
+            {optionsCompanies.length > 1 && (
+              <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }}>
+                <Select
+                  placeholder="Unidade"
+                  data={optionsCompanies}
+                  value={searchCompany}
+                  onChange={setSearchCompany}
+                  searchable
+                  clearable
+                />
+              </Grid.Col>
+            )}
+            <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }}>
+              <TextInput
+                placeholder="Buscar por nome ou e-mail do cliente"
+                leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+                value={search}
+                onChange={event => setSearch(event.target.value)}
+                onBlur={event => setSearchFilter(event.target.value)}
               />
             </Grid.Col>
-          )}
-          {optionsCompanies.length > 1 && (
-            <Grid.Col span={{ base: 12, sm: 6, md: 4 }}>
-              <Select
-                placeholder="Unidade"
-                data={optionsCompanies}
-                value={searchCompany}
-                onChange={setSearchCompany}
+            <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }}>
+              <MultiSelect
+                placeholder="Serviços"
+                data={optionsServices}
                 searchable
-                clearable
+                value={searchServices}
+                onChange={option => setSearchServices(option)}
               />
             </Grid.Col>
-          )}
-          <Grid.Col span={{ base: 12, sm: 6 }}>
-            <TextInput
-              placeholder="Buscar por nome ou e-mail"
-              leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-              value={search}
-              onChange={event => setSearch(event.target.value)}
-              onBlur={event => setSearchFilter(event.target.value)}
-            />
-          </Grid.Col>
-        </Grid>
-        <Divider />
-      </Stack>
+            <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }}>
+              <MultiSelect
+                placeholder="Colaboradores"
+                data={optionsEmployees}
+                searchable
+                value={searchEmployees}
+                onChange={option => setSearchEmployees(option)}
+              />
+            </Grid.Col>
+          </Grid>
+          <Divider />
+        </Stack>
 
-      <Stack pos="relative">
-        <LoadingOverlay
-          visible={loading}
-          zIndex={1000}
-          overlayProps={{ radius: 'sm', blur: 2 }}
-          loaderProps={{ type: 'bars' }}
-        />
-        <ScrollArea h={results.length > 15 ? "55vh" : "auto"} offsetScrollbars>
-          <Table horizontalSpacing="xs" verticalSpacing="xs" miw={700} highlightOnHover>
-            <Table.Tbody>
-              <Table.Tr>
-                <Th>Horário</Th>
-                <Th>Cliente</Th>
-                <Th>Serviço</Th>
-                <Th>Horário</Th>
-                <Th>Confirmado?</Th>
-                <Th>Cancelado?</Th>
-                <Th>Realizado?</Th>
-                <Th>Data Cadastro</Th>
-                <Th>Ações</Th>
-              </Table.Tr>
-            </Table.Tbody>
-            <Table.Tbody>
-              {hourList.map(hour => {
-                const scheduleItems = results.filter(scheduleItem => scheduleItem.start_time === hour)
-                return (
-                  <Fragment key={hour}>
-                    {scheduleItems.length > 0 ? scheduleItems.map((row, index) => {
-                      return (
-                        <Table.Tr key={row.id}>
-                          {index === 0 && <Table.Td className={classes.td} rowSpan={scheduleItems.length}>
-                            <strong>{hour}</strong>
-                          </Table.Td>}
-                          <Table.Td className={classes.td}>
-                            {row.schedule?.client?.name}<br />
-                            {row.schedule?.client?.mobilePhone ? <a href={`tel:${row.schedule?.client?.mobilePhone}`}>{row.schedule?.client?.mobilePhone}</a> : ''}
-                          </Table.Td>
-                          <Table.Td className={classes.td}>{row.service.name} ({row.employee.name})</Table.Td>
-                          <Table.Td className={classes.td}>
-                            {row.start_time} - {row.end_time}
-                          </Table.Td>
-                          <Table.Td className={classes.td}><Display.Status status={row.confirmed} labels={['Sim', 'Não']} /></Table.Td>
-                          <Table.Td className={classes.td}><Display.Status status={row.canceled} labels={['Sim', 'Não']} /></Table.Td>
-                          <Table.Td className={classes.td}><Display.Status status={row.done} labels={['Sim', 'Não']} /></Table.Td>
-                          <Table.Td className={classes.td}>{row.created_at ? dateToHuman(row.created_at) : ''}</Table.Td>
-                          <Table.Td className={classes.td}>
-                            <Group gap="xs">
-                              <Button size="compact-sm" component="a" color="orange" title="Editar" href={`/agendamentos/${row.id}`}>Editar</Button>
-                            </Group>
-                          </Table.Td>
-                        </Table.Tr>
-                      )
-                    }) : (
-                      <Table.Tr className={classes.tr}>
-                        <Table.Td className={classes.td}><strong>{hour}</strong></Table.Td>
-                        <Table.Td className={classes.td} colSpan={8}></Table.Td>
-                      </Table.Tr>
-                    )}
-                  </Fragment>
-                )
-              })}
-              {hourList.length === 0 && (
+        <Stack pos="relative">
+          <LoadingOverlay
+            visible={loading}
+            zIndex={1000}
+            overlayProps={{ radius: 'sm', blur: 2 }}
+            loaderProps={{ type: 'bars' }}
+          />
+          <ScrollArea h={results.length > 15 ? "55vh" : "auto"} offsetScrollbars>
+            <Table horizontalSpacing="xs" verticalSpacing="xs" miw={700} highlightOnHover>
+              <Table.Tbody>
                 <Table.Tr>
-                  <Table.Td colSpan={9}>
-                    <Text fw={500} ta="center">
-                      Nenhum horário disponível
-                    </Text>
-                  </Table.Td>
+                  <Th>Horário</Th>
+                  <Th>Cliente</Th>
+                  <Th>Serviço</Th>
+                  <Th>Horário</Th>
+                  <Th>Confirmado?</Th>
+                  <Th>Cancelado?</Th>
+                  <Th>Realizado?</Th>
+                  <Th>Data Cadastro</Th>
+                  <Th>Ações</Th>
                 </Table.Tr>
-              )}
-            </Table.Tbody>
-          </Table>
-        </ScrollArea>
-      </Stack>
-    </Container>
+              </Table.Tbody>
+              <Table.Tbody>
+                {hourList.map(hour => {
+                  const scheduleItems = results.filter(scheduleItem => scheduleItem.start_time === hour)
+                  return (
+                    <Fragment key={hour}>
+                      {scheduleItems.length > 0 ? scheduleItems.map((row, index) => {
+                        return (
+                          <Table.Tr key={row.id}>
+                            {index === 0 && <Table.Td className={classes.td} rowSpan={scheduleItems.length}>
+                              <strong>{hour}</strong>
+                            </Table.Td>}
+                            <Table.Td className={classes.td}>
+                              {row.schedule?.client?.name}<br />
+                              {row.schedule?.client?.mobilePhone ? <a href={`tel:${row.schedule?.client?.mobilePhone}`}>{row.schedule?.client?.mobilePhone}</a> : ''}
+                            </Table.Td>
+                            <Table.Td className={classes.td}>
+                              {row.employee.name}<br />
+                              {row.service.name}
+                            </Table.Td>
+                            <Table.Td className={classes.td}>
+                              {row.start_time} - {row.end_time}
+                            </Table.Td>
+                            <Table.Td className={classes.td}><Display.Status status={row.confirmed} labels={['Sim', 'Não']} /></Table.Td>
+                            <Table.Td className={classes.td}><Display.Status status={row.canceled} labels={['Sim', 'Não']} /></Table.Td>
+                            <Table.Td className={classes.td}><Display.Status status={row.done} labels={['Sim', 'Não']} /></Table.Td>
+                            <Table.Td className={classes.td}>{row.created_at ? dateToHuman(row.created_at) : ''}</Table.Td>
+                            <Table.Td className={classes.td}>
+                              <Group gap="xs">
+                                <Button size="compact-sm" component="a" color="orange" title="Editar" href={`/agendamentos/${row.id}`}>Editar</Button>
+                              </Group>
+                            </Table.Td>
+                          </Table.Tr>
+                        )
+                      }) : (
+                        <Table.Tr className={classes.tr}>
+                          <Table.Td className={classes.td}><strong>{hour}</strong></Table.Td>
+                          <Table.Td className={classes.td} colSpan={8}></Table.Td>
+                        </Table.Tr>
+                      )}
+                    </Fragment>
+                  )
+                })}
+                {hourList.length === 0 && (
+                  <Table.Tr>
+                    <Table.Td colSpan={9}>
+                      <Text fw={500} ta="center">
+                        Nenhum horário disponível
+                      </Text>
+                    </Table.Td>
+                  </Table.Tr>
+                )}
+              </Table.Tbody>
+            </Table>
+          </ScrollArea>
+        </Stack>
+      </Container>
+
+      <Modal opened={register} onClose={() => setRegister(false)} title="Agendar" centered>
+        <FormSchedule.Basic mutate={mutate} />
+      </Modal>
+    </>
   )
 }
 
